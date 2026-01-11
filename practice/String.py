4df0a75f52863233e0d2adb47bbf45cc2300d713
AanyaNautiyal16 <178308859+AanyaNautiyal16@users.noreply.github.com>
2026-01-11 23:55:27 +0530

    String
    Practicing STring methods, operations and slicing

--- practice/String.py
+++ practice/String.py
@@ -15,4 +15,34 @@ nm="Harry"
 print(nm[-4:-2])
 ''' String Methids in Python '''
 # Strings are immutable
-
+a = "aAnyaa !!! Aanyaa!!"
+print(len(a)) #length
+print(a.upper()) #uppercase
+print(a.lower()) #lowercase
+# removes the trailing part of the string and not the leading part i.e. "!" in this example
+print(a.rstrip("!")) 
+# replacing all the recurrences of aanyaa with aanya
+print(a.replace("Aanyaa" , "Aanya"))
+print(a.split(" "))#splits the string at the given character and returns a list
+print(a.capitalize())#capitalizes the first letter of the string
+print(a.count("!!"))
+print(a.endswith("!!!"))
+b="Aanyaa Nauti is 1 2 yal is a great human being\n"
+print(b.center(30))
+print(len(b))
+print(len(b.center(30)))
+#to check the value in between the string using slicing
+print(b.endswith("Na",5,9))
+# we can overwrite the variable in python but we cannot change the string , we can only create a new string
+print(b.find("is")) #give the index of the first occurance of the given string
+# INDEX METHOD
+print(b.find("ish")) #returns -1 if not found! if we are sure then we can use find and if not use index
+# print(b.index("ish")) #raise expception and ends the program if the string is not found returns substring not found error
+x=" Aanya00 "
+print(x.isalnum())
+print(x.isalpha())
+print(x.islower())
+print(b.isprintable())
+print(x.title())
+print(x.isspace())
+print(x.swapcase())
