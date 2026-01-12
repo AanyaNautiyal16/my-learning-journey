05fb6d1c219f90dcfa7256e52001e7ec758ef723
AanyaNautiyal16 <178308859+AanyaNautiyal16@users.noreply.github.com>
2026-01-12 16:10:32 +0530

    Add examples of for and while loops in Python
    Introduced sample code demonstrating for loops with ranges, string iteration and nested loops as well as a section header for while loops in practice/loops.py.

---
+++ practice/loops.py
@@ -0,0 +1,22 @@
+# for loops in python
+
+
+
+# for k in range(2,11,2): #start,stop and step
+#     print(k)
+
+# a = "Aanya"
+# for character in a:
+#     print(character)
+#     if character == "a":
+#         print("This Character is Speical")
+# Colours = [ "red","green","blue","pink"]
+# for color in Colours:
+#     print(color)
+#     if color=="pink":
+#      print("this is my favourite colour.")
+#     for i in color:
+#        print(i)
+
+# while loops in python
+
