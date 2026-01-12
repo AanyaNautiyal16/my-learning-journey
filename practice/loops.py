65df2ae359a4049c6885c82aa1e3935fb1ef0a3c
AanyaNautiyal16 <178308859+AanyaNautiyal16@users.noreply.github.com>
2026-01-12 17:01:47 +0530

    Add examples of for and while loops with break/continue
    Expanded loops.py to include practical examples of for loops, while loops, and the use of break and continue statements. Demonstrates iterating over ranges, strings, and lists, as well as incrementing and decrementing while loops. Also added illustrative comments for clarity.

--- practice/loops.py
+++ practice/loops.py
@@ -1,22 +1,55 @@
 # for loops in python
 
+for k in range(2,11,2): #start,stop and step
+    print(k)
 
-
-# for k in range(2,11,2): #start,stop and step
-#     print(k)
-
-# a = "Aanya"
-# for character in a:
-#     print(character)
-#     if character == "a":
-#         print("This Character is Speical")
-# Colours = [ "red","green","blue","pink"]
-# for color in Colours:
-#     print(color)
-#     if color=="pink":
-#      print("this is my favourite colour.")
-#     for i in color:
-#        print(i)
+a = "Aanya"
+for character in a:
+    print(character)
+    if character == "a":
+        print("This Character is Speical")
+Colours = [ "red","green","blue","pink"]
+for color in Colours:
+    print(color)
+    if color=="pink":
+     print("this is my favourite colour.")
+    for i in color:
+       print(i)
 
 # while loops in python
+i = 0 #incrementing while loop
+while (i<5):
+    print(i)
+    i=i+1
+    print(f"Done with the while loop {i}\n")
+
+count = 10 #decrementing while loop
+while(count>0):
+    print(count)
+    count=count-1
+print("Happy New Year 2026!\n")
+
+# break and continue statements
+# break = loop ko chodkar nikal jao 
+# continue =  uss iteration ko chodkar nikal jao
+for i in range(11):
+    if(i==10):
+        break
+    print("5 X ",i+1,"=",5*(i+1))
+print("LOL! LOOP GOT BROKEN\n")
+
+for i in range(12):
+    if(i==10):
+     print("LOL! A ITERATION JUST GOT SKIPPED")
+     continue
+    print("5 X ",i+1,"=",5*(i+1))
+    
+while True:
+   print(i)
+   i=i+1
+   if(i%100==0):
+    break
+
+
+
 
